b460770eeb7597ccb5be5b1d5a6462f2a7233b90
Newbion17 <nyubion673@gmail.com>
2025-01-23 17:27:04 +0900

    Add files via upload

---
+++ assets/shared/custom_events/Two_Flashes.lua
@@ -0,0 +1,20 @@
+function onEvent(n,v1,v2)
+
+
+	if n == 'Two_Flashes' then
+
+	   makeLuaSprite('flash', '', 0, 0);
+        makeGraphic('flash',1500,900,v2)
+	      setLuaSpriteScrollFactor('flash',0,0)
+		  addLuaSprite('flash', true);
+		  setProperty('flash.scale.y',2)
+	      setProperty('flash.scale.x',2)
+	      setProperty('flash.alpha',1)
+		setProperty('flash.alpha',1)
+		setBlendMode('flash', 'add')
+		doTweenAlpha('flTw','flash',0,v1,'linear')
+	end
+
+ 
+
+end
